5bda2b96f6a2f3704b82441724aefdbe1ad1902f
Sourav Kumar Parida <souravparida323@gmail.com>
2025-09-03 00:49:38 +0530

    Fix relative import for Render deployment

--- BHP/server/app.py
+++ BHP/server/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import util  # your utility file
+from . import util # your utility file
 from contextlib import asynccontextmanager
 
 # -------------------------------
